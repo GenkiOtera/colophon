b1d946c1cd3114d68d9d91def566c28d59cd4746
KINGE <g.otera.development@gmail.com>
2022-02-08 16:25:00 +0900

    routingを整備

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
+import { AreasComponent } from './components/areas/areas.component';
+import { CropsComponent } from './components/crops/crops.component';
+import { EncyclopediaComponent } from './components/encyclopedia/encyclopedia.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component:AuthComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'areas', component:AreasComponent},
+  {path: 'crops', component:CropsComponent},
+  {path: 'encyclopedia', component:EncyclopediaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
