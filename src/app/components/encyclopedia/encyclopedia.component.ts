7fea1219e9fd93b9f599a8007bcd11494b5bc78f
KINGE <77057831+GenkiOtera@users.noreply.github.com>
2022-02-11 02:00:58 +0900

    [WIP]ずかんコンポーネントを作成

--- src/app/components/encyclopedia/encyclopedia.component.ts
+++ src/app/components/encyclopedia/encyclopedia.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-encyclopedia',
-  template: `
-    <p>
-      encyclopedia works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: 'encyclopedia.component.html',
+  styleUrls: ['encyclopedia.component.css'],
 })
 export class EncyclopediaComponent implements OnInit {
+
+  seasons: {[key:number]: string} = {
+    0:'すべて',
+    1:'はる',
+    2:'なつ',
+    3:'あき',
+    4:'ふゆ',
+  }
+  selectedSeason = this.seasons[0];
 
   constructor() { }
 
