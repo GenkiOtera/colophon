e99226dfcd265ad24e4460f8c99bceb90d14476f
KINGE <g.otera.development@gmail.com>
2022-02-15 16:50:40 +0900

    area画面の作成

--- src/app/components/areas/areas.component.ts
+++ src/app/components/areas/areas.component.ts
@@ -2,19 +2,49 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-areas',
-  template: `
-    <p>
-      area works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: 'areas.component.html',
+  styleUrls: ['areas.component.css']
 })
 export class AreasComponent implements OnInit {
+
+  areas = [
+    'きた１'
+    , 'きた２'
+    , 'みなみ１'
+    , 'みなみ２'
+    , 'みなみ３'
+    , 'ちゅうおう１'
+    , 'ちゅうおう２'
+    , 'ひがし１'
+    , 'にし１'
+    , 'にし２'
+    ,'きた１'
+    , 'きた２'
+    , 'みなみ１'
+    , 'みなみ２'
+    , 'みなみ３'
+    , 'ちゅうおう１'
+    , 'ちゅうおう２'
+    , 'ひがし１'
+    // , 'にし１'
+    // , 'にし２'
+    // ,'きた１'
+    // , 'きた２'
+    // , 'みなみ１'
+    // , 'みなみ２'
+    // , 'みなみ３'
+    // , 'ちゅうおう１'
+    // , 'ちゅうおう２'
+    // , 'ひがし１'
+    // , 'にし１'
+    // , 'にし２'
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  openDialog(){
 
+  }
 }
