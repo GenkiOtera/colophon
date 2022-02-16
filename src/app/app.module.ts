82eb2fe95ccec0ccebb42f99bf2135c5fecb07c2
KINGE <g.otera.development@gmail.com>
2022-02-16 16:49:38 +0900

    Revert "PWAを導入"
    This reverts commit ca78f0f3189ce5c5498c17eb7e065ba24888973e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
@@ -52,12 +51,6 @@ import { EncyclopediaComponent } from './components/encyclopedia/encyclopedia.co
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
 
     // firebase
     AngularFireModule.initializeApp(environment.firebase),
