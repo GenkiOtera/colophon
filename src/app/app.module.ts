ca78f0f3189ce5c5498c17eb7e065ba24888973e
KINGE <g.otera.development@gmail.com>
2022-02-16 10:14:28 +0900

    PWAを導入
    ngCLIで導入しただけの状態

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
@@ -53,6 +54,12 @@ import { EncyclopediaComponent } from './components/encyclopedia/encyclopedia.co
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
 
     // firebase
     provideFirebaseApp(() => initializeApp(environment.firebase)),
